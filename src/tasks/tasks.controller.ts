128ddd0b40a1c2de86b1b5105c387050b272a38a
ngirimana <chadrackngirimana@gmail.com>
2023-06-22 23:03:00 +0200

    controller,service nad DTO for task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Get, Post } from '@nestjs/common';
 import { TasksService } from './tasks.service';
+import { Task } from './task.model';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
+
+  @Get()
+  getAllTasks() {
+    return this.tasksService.getAllTasks();
+  }
+
+  @Post()
+  createTask(@Body('title') title, @Body('description') description): Task {
+    return this.tasksService.createTask(title, description);
+  }
 }
